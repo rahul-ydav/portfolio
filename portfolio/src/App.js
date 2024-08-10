b3634a489e17d5231ffe59c1e6ceb96c3e3c5281
rahulydv <ry040799@gmail.com>
2024-08-10 16:38:47 +0530

    font fix

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -7,6 +7,7 @@ import {ReactComponent as RedisIcon} from './assets/redisicon.svg';
 import {ReactComponent as KafkaIcon} from './assets/kafkaicon.svg';
 import {ReactComponent as PostgresIcon} from './assets/postgresicon.svg';
 import MyPic from "./assets/mypic.jpg";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const helper = {
   key: Math.random(),
@@ -21,37 +22,51 @@ function App() {
   const icons = [NodeIcon, JSIcon, RedisIcon, KafkaIcon, PostgresIcon, ReactIcon];
 
   return (
-    <div className="container">
-      <section className='overview'>
-      {/* <h1>Overview</h1> */}
-        
-        <div className='overview-text'>
-          <p id="overviewcontent">I am 3 years experienced software developer having good command in JavaScript, Node.js, React.js, Java, PostgreSQL. During this period, I wrote a lot of industrial level code and most of my work is on production.</p>
-          <img id="mypic" src= {MyPic} alt="No profile pic"/>
-          <div id="techStackIcons">
-          {
-            icons.map(Icon => {
-              return (<div
-                className = 'float'
-                // style={{ position: 'absolute',top: helper.initialy(), right: helper.initialx(), rotate: helper.initialTilt()} }
-              >
-                <Icon/>
-              </div>)
-            })
-          }
-        </div>
-        </div>
+    <div className='topEle'>
 
-      </section>
-      <section className='experience'>
-        <h1>Experience</h1>
-      </section>
-      <section className='projects'>
-        <h1>Projects</h1>
-      </section>
-      <section className='education'>
-        <h1>Educational BackGround</h1>
-      </section>
+      
+      <Sidebar/>
+      <div className="container">
+        <section className='overview'>
+        {/* <h1>Overview</h1> */}
+        
+          
+          <div className='overview-text'>
+            <div className="firstBlock">
+              <div><img id="mypic" src= {MyPic} alt="No profile pic"/></div>
+              <div className="overviewAllText">
+                <h1 id="nameInOverview">Rahul Yadav</h1>
+                <p class="overviewSpan" style={{'fontSize': '1rem'}}>Full-Stack Developer/ Senior Software Engineer</p>
+                <p class="overviewSpan">with expertise in Node.js, React.js, PostgreSQL, Redis, and Kafka. Proven track record in designing scalable solutions and optimizing performance</p>
+                {/* <p id="overviewcontent">I am 3 years experienced software developer having good command in JavaScript, Node.js, React.js, Java, PostgreSQL. During this period, I wrote a lot of industrial level code and most of my work is on production.</p> */}
+              </div>
+            </div>
+            <div id="techStackIcons">
+            {
+              icons.map(Icon => {
+                return (<div
+                  className = 'float'
+                  // style={{ position: 'absolute',top: helper.initialy(), right: helper.initialx(), rotate: helper.initialTilt()} }
+                >
+                  <Icon/>
+                </div>)
+              })
+            }
+          </div>
+          </div>
+
+        </section>
+        <section className='experience'>
+          <h1>I am 3 years experienced software developer having good command in JavaScript, Node.js, React.js, Java, PostgreSQL. During this period, I wrote a lot of industrial level code and most of my work is on production.</h1>
+          {/* <p id="overviewcontent"></p> */}
+        </section>
+        <section className='projects'>
+          <h1>Projects</h1>
+        </section>
+        <section className='education'>
+          <h1>Educational BackGround</h1>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ portfolio/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import './Sidebar.css';
+import {ReactComponent as LinkedInIcon} from './assets/linkedin.svg';
+import {ReactComponent as GithubIcon} from './assets/github.svg';
+import {ReactComponent as GmailIcon} from './assets/gmail.svg';
+import {ReactComponent as WhatsappIcon} from './assets/whatsapp.svg';
+
+function Sidebar(){
+    const linkedInLink = "https://www.linkedin.com/in/rahulydv559";
+    const githubLink = "https://github.com/rahul-ydav";
+    const gmailAdd = "rahulydv559@gmail.com";
+    return (
+        <div className='sideBar'>
+            <a href={linkedInLink} target="_blank"><LinkedInIcon/></a>
+            <a href={githubLink} target="_blank"><GithubIcon/></a>
+            <a href={gmailAdd} target="_blank"><GmailIcon/></a>
+            <a href={linkedInLink} target="_blank"><WhatsappIcon/></a>
+        </div>
+    );
+}
+export default Sidebar;
