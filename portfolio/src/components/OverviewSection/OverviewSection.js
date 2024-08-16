ba3d2fdaa50d32161cef586e0111f49e57cd7a53
rahulydv <ry040799@gmail.com>
2024-08-16 10:22:57 +0530

    rearranged bubbles

--- portfolio/src/components/OverviewSection/OverviewSection.js
+++ portfolio/src/components/OverviewSection/OverviewSection.js
@@ -23,30 +23,30 @@ function OverviewSection(){
       {/*  animated backgroung */}
       <div class="bubbles">
         <span style={{"--i":"11"}}></span>
-        <span style={{"--i":"12"}}></span>
+        <span style={{"--i":"5"}}></span>
         <span style={{"--i":"15"}}></span>
         <span style={{"--i":"25"}}></span>
+        <span style={{"--i":"35"}}></span>
         <span style={{"--i":"21"}}></span>
         <span style={{"--i":"19"}}></span>
+        <span style={{"--i":"34"}}></span>
+        <span style={{"--i":"3"}}></span>
         <span style={{"--i":"16"}}></span>
+        <span style={{"--i":"33"}}></span>
         <span style={{"--i":"18"}}></span>
         <span style={{"--i":"28"}}></span>
         <span style={{"--i":"19"}}></span>
         <span style={{"--i":"22"}}></span>
+        <span style={{"--i":"2"}}></span>
         <span style={{"--i":"14"}}></span>
         <span style={{"--i":"24"}}></span>
         <span style={{"--i":"17"}}></span>
+        <span style={{"--i":"12"}}></span>
         <span style={{"--i":"29"}}></span>
         <span style={{"--i":"13"}}></span>
         <span style={{"--i":"20"}}></span>
-        <span style={{"--i":"3"}}></span>
-        <span style={{"--i":"33"}}></span>
-        <span style={{"--i":"4"}}></span>
-        <span style={{"--i":"34"}}></span>
-        <span style={{"--i":"5"}}></span>
-        <span style={{"--i":"35"}}></span>
-        <span style={{"--i":"2"}}></span>
         <span style={{"--i":"32"}}></span>
+        <span style={{"--i":"4"}}></span>
       </div>
         
           
